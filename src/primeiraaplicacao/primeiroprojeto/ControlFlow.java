066d47568cf2f3abd98fc8ac57c008afa9d123c0
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-04-03 09:11:07 -0300

    implementa a variavel da sinopse

--- src/primeiraaplicacao/primeiroprojeto/ControlFlow.java
+++ src/primeiraaplicacao/primeiroprojeto/ControlFlow.java
@@ -1,4 +1,7 @@
 package primeiraaplicacao.primeiroprojeto;
 
 public class ControlFlow {
+    public static void main(String[] args) {
+
+    }
 }
--- src/primeiraaplicacao/primeiroprojeto/StreamApp.java
+++ src/primeiraaplicacao/primeiroprojeto/StreamApp.java
@@ -4,5 +4,24 @@ public class StreamApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Screen Match. Your new streaming platform.");
         System.out.println("Movie: Top Gun Maverick");
+
+        String movieName = "Top Gun Maverick";
+        int reliaseYear = 2022;
+        System.out.println("Realise year: " + reliaseYear);
+        boolean isIncludedInPlan = true;
+        double movieRating = 8.1;
+
+        // Average calculated from the 3 user ratings
+        double averageMovieRating = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(averageMovieRating);
+
+        String synopsis = """
+                SYNOPSIS
+                Movie: %s
+                Realise year: %d
+                Movie rating: %.1f
+                Average movie rating: %.1f
+                """.formatted(movieName, reliaseYear, movieRating, averageMovieRating);
+        System.out.println(synopsis);
     }
 }
