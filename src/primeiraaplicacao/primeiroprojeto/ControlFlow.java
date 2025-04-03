e32cb3830ff194c0b2618a10c2490aed4dcf1b2e
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-04-03 09:35:33 -0300

    implementa controle de fluxo

--- src/primeiraaplicacao/primeiroprojeto/ControlFlow.java
+++ src/primeiraaplicacao/primeiroprojeto/ControlFlow.java
@@ -3,5 +3,22 @@ package primeiraaplicacao.primeiroprojeto;
 public class ControlFlow {
     public static void main(String[] args) {
 
+        int realiseYear = 2022;
+        double movieRating = 8.1;
+        boolean isIncludedInPlan = true;
+        String subscriptionPlanType = "premium";
+
+        if ( realiseYear >= 2022) {
+            System.out.println("The movie is a new release and currently popular. ");
+        } else {
+            System.out.println("A classic retro film - highly recommended");
+        }
+
+        if (isIncludedInPlan && subscriptionPlanType.equals("premium")) {
+            System.out.println("Unlocked content");
+        } else {
+            System.out.println("Content available for rent");
+        }
+
     }
 }
