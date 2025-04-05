ee85e92c55240b223675ec91efd0ae1bd2952e5d
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-04-05 18:35:30 -0300

    Cria o pacote do projeto screen match. Cria a classe com os atributos

---
+++ src/screenmatch/Movie.java
@@ -0,0 +1,13 @@
+package screenmatch;
+
+public class Movie {
+
+    String movieName;
+    int realiseYear;
+    boolean isIncludedInPlan;
+    double movieRating;
+    int averageMovieRating;
+    int runTimeMinutes;
+
+
+}
