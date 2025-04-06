c377a5a4208b58ad5dc6dc25706d2d981c35ca20
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-04-05 21:01:34 -0300

    implementa os getters e setters. Insere os modificadores de acesso. Cria pacotes e arquivo .txt

--- src/screenmatch/Main.java
+++ src/screenmatch/Main.java
@@ -1,13 +1,15 @@
 package screenmatch;
 
+import screenmatch.br.com.alura.screenmatch.modelos.Movie;
+
 public class Main {
     public static void main(String[] args) {
 
         Movie filme1 = new Movie();
-        filme1.movieName = "First Blood";
-        filme1.realiseYear = 1982;
-        filme1.runTimeMinutes = 193;
-        filme1.isIncludedInPlan = true;
+        filme1.setMovieName("First Blood");
+        filme1.setRealiseYear(1982);
+        filme1.setRunTimeMinutes(193);
+        filme1.setIncludedInPlan(true);
 
         filme1.calculateTotalRating(5);
         filme1.calculateTotalRating(10);
