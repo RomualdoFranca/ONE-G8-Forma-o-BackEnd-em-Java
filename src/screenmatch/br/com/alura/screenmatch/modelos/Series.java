907fcf177b030408bc78f714ac6fa15ba95b3356
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-04-07 10:13:00 -0300

    cria as classes titulo e serie.Implementa os atributos, getters e setters da classe serie

---
+++ src/screenmatch/br/com/alura/screenmatch/modelos/Series.java
@@ -0,0 +1,40 @@
+package screenmatch.br.com.alura.screenmatch.modelos;
+
+public class Series {
+    private int seasonCount;
+    private boolean isAvailableOnStream;
+    private int episodesPerSeason;
+    private int episodeDurationMinutes;
+
+    public int getSeasonCount() {
+        return seasonCount;
+    }
+
+    public void setSeasonCount(int seasonCount) {
+        this.seasonCount = seasonCount;
+    }
+
+    public boolean isAvailableOnStream() {
+        return isAvailableOnStream;
+    }
+
+    public void setAvailableOnStream(boolean availableOnStream) {
+        isAvailableOnStream = availableOnStream;
+    }
+
+    public int getEpisodesPerSeason() {
+        return episodesPerSeason;
+    }
+
+    public void setEpisodesPerSeason(int episodesPerSeason) {
+        this.episodesPerSeason = episodesPerSeason;
+    }
+
+    public int getEpisodeDurationMinutes() {
+        return episodeDurationMinutes;
+    }
+
+    public void setEpisodeDurationMinutes(int episodeDurationMinutes) {
+        this.episodeDurationMinutes = episodeDurationMinutes;
+    }
+}
---
+++ src/screenmatch/br/com/alura/screenmatch/modelos/Title.java
@@ -0,0 +1,85 @@
+package screenmatch.br.com.alura.screenmatch.modelos;
+
+public class Title {
+
+    private  String movieName;
+    private int realiseYear;
+    private  boolean isIncludedInPlan;
+    private double movieRating;
+    private double ratingCount;
+    int runTimeMinutes;
+
+    public String getMovieName() {
+
+        return movieName;
+    }
+
+    public int getRealiseYear() {
+
+        return realiseYear;
+    }
+
+    public boolean isIncludedInPlan() {
+
+        return isIncludedInPlan;
+    }
+
+    public double getRatingCount() {
+
+        return ratingCount;
+    }
+
+    public int getRunTimeMinutes() {
+
+        return runTimeMinutes;
+    }
+
+    public double getMovieRating() {
+
+
+        return movieRating;
+    }
+
+    public void setMovieName(String movieName) {
+
+        this.movieName = movieName;
+    }
+
+    public void setRealiseYear(int realiseYear) {
+
+        this.realiseYear = realiseYear;
+    }
+
+    public void setIncludedInPlan(boolean includedInPlan) {
+
+        isIncludedInPlan = includedInPlan;
+    }
+
+    public void setRunTimeMinutes(int runTimeMinutes) {
+        this.runTimeMinutes = runTimeMinutes;
+    }
+
+    public void displayMovieDetails() {
+        String movieDetails = """
+                Movie Details
+                Title: %s
+                Realise year: %d
+                Movie rating: %.1f
+                Average movie rating: %.1f
+                Is included in plan: %s
+                Run time minutes: %d
+                """.formatted(movieName, realiseYear,  movieRating, calculateAverage(), isIncludedInPlan ,runTimeMinutes);
+        System.out.println(movieDetails);
+    }
+
+    public void calculateTotalRating(double rating) {
+        movieRating += rating;
+        ratingCount++;
+    }
+
+    public double calculateAverage() {
+
+        return movieRating / ratingCount;
+    }
+
+}
