fecde734960965aea72ef96d6e181e965ab362e0
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-04-13 20:45:53 -0300

    cria a classe main, implementa os objetos e testa os metodos

--- src/primeiraaplicacao/horadapratica/modulo04/calculararea/Main.java
+++ src/primeiraaplicacao/horadapratica/modulo04/calculararea/Main.java
@@ -2,6 +2,13 @@ package primeiraaplicacao.horadapratica.modulo04.calculararea;
 
 public class Main {
     public static void main(String[] args) {
-        
+        CalculadoraSalaRentangular sala1 = new CalculadoraSalaRentangular();
+        sala1.setMedidaAltura(3.0);
+        sala1.setMedidaLargura(2.9);
+
+        double area = sala1.calcularArea();
+        double perimetro = sala1.calcularPerimetro();
+        System.out.printf("Medidas:\nLargura: %.2f\nAltura: %.2f\nPerímetro: %.2f\nÁrea: %.2f\n"
+                ,sala1.getMedidaLargura(), sala1.getMedidaAltura(), perimetro, area);
     }
 }
