bda94fbb3d5c8dd1d34963f04eeee09367740311
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-04-14 21:56:05 -0300

    inicia o desafio do minhasmusicas

---
+++ src/br/com/alura/minhasmusicas/modelos/Audio.java
@@ -0,0 +1,38 @@
+package br.com.alura.minhasmusicas.modelos;
+
+public class Audio {
+    private String titulo;
+    private int totalReproducoes;
+    private int totalCurtidas;
+    private double classificacao;
+
+    public void curte() {
+        this.totalCurtidas ++;
+
+    }
+    public void reproduz(){
+        this.totalReproducoes ++;
+    }
+
+    public double getClassificacao() {
+        return classificacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+}
