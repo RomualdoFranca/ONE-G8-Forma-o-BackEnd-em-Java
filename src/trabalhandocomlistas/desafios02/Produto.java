3e7d6288dd13a26866c9536b69bd1ca400e3e593
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-04-28 08:06:49 -0300

    cria a classeProduto com atributos, metodo toString() e construtor.Cria classe principal e arquivo de texto com a descrição do desafio

---
+++ src/trabalhandocomlistas/desafios02/Produto.java
@@ -0,0 +1,34 @@
+package trabalhandocomlistas.desafios02;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                Nome do produto:        %s
+                Preço:              R$%.2f
+                Quantidade:             %d unidade(s)
+                """;
+    }
+}
