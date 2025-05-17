08e2e2d112a83cfc23b5133d41c8d51a76208553
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-05-17 14:04:25 -0300

    something...

--- src/trabalhandocomlistas/novaaplicacao/controledecompras/Compras.java
+++ src/trabalhandocomlistas/novaaplicacao/controledecompras/Compras.java
@@ -6,6 +6,7 @@ public class Compras {
     private String nomeItem;
     private double preco;
     private double limiteCartao;
+    private double somaTotal;
 
 //    public Compras(String nomeItem, double preco) {
 //        this.nomeItem = nomeItem;
@@ -24,15 +25,19 @@ public class Compras {
         return limiteCartao;
     }
 
+    public double getSomaTotal() {
+        return somaTotal;
+    }
+
     Scanner scanner = new Scanner(System.in);
 
     public void menuLancamentoCompras() {
         int opcao;
-
         System.out.println("Digite o limite do cartão de crédito.");
         this.limiteCartao = scanner.nextDouble();
         scanner.nextLine();
-        
+
+
         do {
 
             System.out.println("Digite a descrição da compra:");
@@ -40,6 +45,8 @@ public class Compras {
 
             System.out.println("Digite o valor da compra:");
             this.preco = scanner.nextDouble();
+            somaCompras(this.preco);
+            scanner.nextLine();
 
             System.out.println("Compra realizada!");
             String msg = toString();
@@ -47,20 +54,40 @@ public class Compras {
 
             System.out.println("Digite 0 para sair ou 1 para continuar");
             opcao = scanner.nextInt();
+            scanner.nextLine();
 
-        } while (opcao != 0);
+        } while (opcao != 0 );
 
         System.out.println("Saindo da aplicação");
-        toString();
+
 
     }
 
-
+    @Override
     public String toString() {
         return """
                 Limite do cartão: R$%.2f
                 Descrição do produto: %s
                 Valor do produto: %.2f
-                """.formatted(getLimiteCartao(), getNomeItem(), getPreco());
+                Valor total das compras: %.2f
+                """.formatted(getLimiteCartao(), getNomeItem(), getPreco(), getSomaTotal());
+    }
+
+    // metodo que soma os valores das compras
+    public void somaCompras(double valor) {
+        double somaParcial = 0;
+        somaParcial += valor;
+
+        if (somaParcial <= limiteCartao) {
+            this.somaTotal += somaParcial;
+        }else {
+            System.out.println("Compra não realizada\nSaldo insuficiente:");
+        }
+    }
+
+    public void verificaLimite() {
+        if(somaTotal <= limiteCartao) {
+
+        }
     }
 }
--- src/trabalhandocomlistas/novaaplicacao/controledecompras/Principal.java
+++ src/trabalhandocomlistas/novaaplicacao/controledecompras/Principal.java
@@ -4,6 +4,5 @@ public class Principal {
     public static void main(String[] args) {
         Compras compras01 = new Compras();
         compras01.menuLancamentoCompras();
-        System.out.println(compras01.getLimiteCartao());
     }
 }
